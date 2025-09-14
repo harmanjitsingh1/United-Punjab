c2f1ad809cacadc98b952fb0ffc52bc3d3f263da
Harmanjit Singh <harmanjitsingh3002@gamil.com>
2025-09-15 00:08:27 +0530

    Fix folder and file names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
 import "./i18n";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Layout from "./Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import LoginPage from "./Pages/LoginPage.jsx";
-import SignupPage from "./Pages/SignupPage.jsx";
-import ServicesPage from "./Pages/ServicesPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
-import NotFound from "./Pages/NotFound.jsx";
-import LanguageSwitcher from "./Components/LanguageSwitcher.jsx";
-import ContactUsPage from "./Pages/ContactUsPage.jsx";
-import ScholarshipsPage from "./Pages/ScholarshipsPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import ContactUsPage from "./pages/ContactUsPage.jsx";
+import ScholarshipsPage from "./pages/ScholarshipsPage.jsx";
+import SportsClubPage from "./pages/SportsClubPage.jsx";
+import BusinessPage from "./pages/BusinessPage.jsx";
+import InitiativesPage from "./pages/InitiativesPage.jsx";
+import OtpVerification from "./pages/OtpVerification.jsx";
+import GurmukhiSeriesPage from "./pages/GurmukhiSeriesPage.jsx";
+import UserFormWizard from "./pages/Temp.jsx";
+
 import Services from "./Components/Services.jsx";
-import SportsClubPage from "./Pages/SportsClubPage.jsx";
-import BusinessPage from "./Pages/BusinessPage.jsx";
-import InitiativesPage from "./Pages/InitiativesPage.jsx";
 import Header from "./Components/Header.jsx";
-import OtpVerification from "./Pages/OtpVerification.jsx";
-import GurmukhiSeriesPage from "./Pages/GurmukhiSeriesPage.jsx";
-import UserFormWizard from "./Pages/Temp.jsx";
+import LanguageSwitcher from "./Components/LanguageSwitcher.jsx";
 import ProtectedRoute from "./Components/ProtectedRoute.jsx";
+
 import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
